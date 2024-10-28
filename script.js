174bcbe369bfcc62c241043177099090cb47f6c8
Flora Nwakpa <floranwakpa@gmail.com>
2024-10-28 14:40:19 +0100

    added new functionality

--- script.js
+++ script.js
@@ -8,8 +8,12 @@ const qrCodeHolder =  document.getElementById('qr-code')
 // function to generate Qr code
 
 function generateQrCode() {
-    imageHolder.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + qrText.value;
+   if(qrText.value != '') {
+     imageHolder.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + qrText.value;
     qrCodeHolder.style.display = 'block'
+   } else {
+    alert ('Kindly enter text or url')  // Runs when the input field is empty
+   }
 }
 
 
